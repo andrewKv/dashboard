777c00e2b5401b22feed0fcae132bd4ede9b3953
Andrew <andrew-keys@outlook.com>
2021-10-19 15:30:43 +0100

    Commit initial files

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+
+
+// render Login component, dashboard
+ReactDOM.render(
+  <React.StrictMode>
+    <Login /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
